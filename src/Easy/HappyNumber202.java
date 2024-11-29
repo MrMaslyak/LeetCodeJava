6cb7c046be6235dbe32e2aba33c6f363fc97ccb9
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-11-29 18:43:45 +0200

    add easy task happuNumber202

--- src/Easy/HappyNumber202.java
+++ src/Easy/HappyNumber202.java
@@ -1,4 +1,29 @@
 package Easy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber202 {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> seenNumbers = new HashSet<>();
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = sumOfSquares(n);
+        }
+        return n == 1;
+    }
+
+    private static int sumOfSquares(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += digit * digit;
+            num /= 10;
+        }
+        return sum;
+    }
 }
