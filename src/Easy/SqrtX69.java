cc4deae9be4c4585f84a6b697dfde6ae0fc69d5e
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-11-29 18:52:54 +0200

    add easy task Sqrt(x)69 not done well

---
+++ src/Easy/SqrtX69.java
@@ -0,0 +1,13 @@
+package Easy;
+
+public class SqrtX69 {
+
+    public static void main(String[] args) {
+        System.out.println(mySqrtWithOperExponentFunc(8));
+    }
+
+    public static int mySqrtWithOperExponentFunc (int x) {
+        Math.sqrt(x);
+        return (int) Math.sqrt(x);
+    }
+}
