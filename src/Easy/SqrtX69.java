792cb8e86c4ffdabb14aea64ffbc5d61ed7a8996
Yuliia Masliana <yuliia.masliana@Admins-MacBook-Air-4.local>
2024-11-29 20:22:49 +0200

    add easy task sqrtX69

--- src/Easy/SqrtX69.java
+++ src/Easy/SqrtX69.java
@@ -4,10 +4,34 @@ public class SqrtX69 {
 
     public static void main(String[] args) {
         System.out.println(mySqrtWithOperExponentFunc(8));
+        System.out.println(mySqrt(2147483647));
     }
 
     public static int mySqrtWithOperExponentFunc (int x) {
         Math.sqrt(x);
         return (int) Math.sqrt(x);
     }
+
+    public static int mySqrt(int x) {
+        if (x == 0 || x == 1) {
+            return x;
+        }
+
+        int left = 1, right = x;
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid <= x / mid) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
 }
