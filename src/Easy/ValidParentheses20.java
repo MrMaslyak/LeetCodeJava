9f2eab72320c58262cc37a1da779800f665b717d
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-12-06 23:18:33 +0200

    lol i don	 do nothing in this but i want active in contribution activity in github XDXD

--- src/Easy/ValidParentheses20.java
+++ src/Easy/ValidParentheses20.java
@@ -2,7 +2,7 @@ package Easy;
 
 public class ValidParentheses20 {
     public static void main(String[] args) {
-        System.out.println(isValid("([)]"));
+        System.out.println(isValid("()[]"));
     }
 
     public static boolean isValid(String s) {
