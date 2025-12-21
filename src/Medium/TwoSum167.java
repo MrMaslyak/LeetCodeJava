a163836dbfd34e18ac9e1e9d09fd8f47cd22f809
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-12-21 19:46:53 +0200

    new era: add TwoSum167

--- src/Medium/TwoSum167.java
+++ src/Medium/TwoSum167.java
@@ -12,17 +12,16 @@ public class TwoSum167 {
     public static int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1 ;
-        int[] answer = new int[1];
         while(left < right){
             int sum = numbers[left] + numbers[right];
-            if (sum < target){
+            if(sum == target){
+                return new int[]{left + 1, right + 1};
+            } else if(sum < target){
                 left++;
-            }else {
+            } else {
                 right--;
             }
-            if (sum == target){
-                return answer;
-            }
         }
+        return null;
     }
 }
