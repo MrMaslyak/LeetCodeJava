37ac1a79d1642299e2e74cfa0da573390a3cb73f
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-12-30 16:42:43 +0200

    first hard task - pizdec nah 76

--- src/Hard/MinimumWindowSubstring76.java
+++ src/Hard/MinimumWindowSubstring76.java
@@ -6,30 +6,55 @@ public class MinimumWindowSubstring76 {
 
     public static void main(String[] args) {
         System.out.println(
-                minWindow("ADOBECODEBANC", "ABC")
+                minWindow("ab", "b")
         );
     }
 
     public static String minWindow(String s, String t) {
+
         int minLength = Integer.MAX_VALUE;
         int minLeft = 0;
-        int minRight = 0;
-        int right = 0;
-        int left = 0;
-        HashMap<Character, Integer> map = new HashMap<>();
-        while (right < s.length()){
+        int left = 0, right = 0;
+        int formed = 0;
+
+        HashMap<Character, Integer> window = new HashMap<>();
+        HashMap<Character, Integer> valid = new HashMap<>();
+
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            valid.put(ch, valid.getOrDefault(ch, 0) + 1);
+        }
+
+        while (right < s.length()) {
             char ch = s.charAt(right);
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if (valid.containsKey(ch)) {
+                window.put(ch, window.getOrDefault(ch, 0) + 1);
+                if (window.get(ch).intValue() == valid.get(ch).intValue()) {
+                    formed++;
+                }
+            }
             right++;
-            if (right < minLength){
-                minLength = s.length();
-                minLeft = left;
-                minRight = right;
-                map.remove(s.charAt(left));
+
+
+            while (formed == valid.size()) {
+                char leftChar = s.charAt(left);
+
+                if (valid.containsKey(leftChar)) {
+                    window.put(leftChar, window.get(leftChar) - 1);
+                    if (window.get(leftChar) < valid.get(leftChar)) {
+                        formed--;
+                    }
+                    if (right - left < minLength) {
+                        minLength = right - left;
+                        minLeft = left;
+                    }
+                }
                 left++;
             }
         }
 
-        return s.substring(minLeft, minRight);
+
+        return minLength == Integer.MAX_VALUE ? "" : s.substring(minLeft, minLeft + minLength);
     }
 }
