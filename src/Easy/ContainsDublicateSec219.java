0f383d80bd6c83540e44a703a2619ce5c2342dc8
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2026-01-06 18:07:36 +0200

    add task 219

---
+++ src/Easy/ContainsDublicateSec219.java
@@ -0,0 +1,30 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class ContainsDublicateSec219 {
+    public static void main(String[] args) {
+        System.out.println(
+                containsNearbyDuplicate(
+                        new int[]{1, 2, 3, 1}, 3
+                )
+        );
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+          if (map.containsKey(num)){
+              int index = map.get(num);
+              if (i - index <= k) {
+                  return true;
+              }
+          }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
