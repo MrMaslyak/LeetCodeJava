7d21b7d9d0bf1fccf2b6fb56e413c57426bc818b
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2026-01-07 18:03:14 +0200

    add 27 task

---
+++ src/Easy/RemoveElementttt27.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class RemoveElementttt27 {
+
+    public static void main(String[] args) {
+        System.out.println(
+                removeElement(new int[]{3, 2, 2, 3}, 3)
+        );
+    }
+
+
+    public static int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
